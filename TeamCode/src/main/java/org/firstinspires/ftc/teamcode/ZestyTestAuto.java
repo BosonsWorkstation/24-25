638eb5d20efe57914af361cc00549187f8de0066
Robotics <robotics@springboro.org>
2024-08-02 13:56:22 -0400

    Initial roadrunner files 8/2/24

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ZestyTestAuto.java
@@ -0,0 +1,327 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.TouchSensor;
+
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.controls.ExposureControl;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.controls.GainControl;
+import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.tfod.TfodProcessor;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+@Autonomous
+public class ZestyTestAuto extends LinearOpMode {
+    private static final boolean USE_WEBCAM = true;
+    private static final String TFOD_MODEL_ASSET = "RedCylinder.tflite";
+    private static final String[] LABELS = {
+            "Red",
+    };
+    private TfodProcessor tfod;
+
+    private AprilTagProcessor aprilTag;
+
+    private VisionPortal visionPortal;
+    private VisionPortal tfodVisionPortal;
+    public SampleMecanumDrive sampleMecanumDrive;
+    private static final SampleMecanumDrive.DirectionEnum direction = SampleMecanumDrive.DirectionEnum.WEST;
+    protected TouchSensor touchSensor = null;
+    //    protected ColorSensor colorSensor1 = null;
+
+    protected DcMotor left_slide_motor = null;
+    protected DcMotor right_slide_motor = null;
+    protected Servo droneLauncher = null;
+    protected Servo armServo = null;
+    protected Servo claw = null;
+    int id1x = 0;
+    int id2x = 0;
+    int id3x = 0;
+    int id4x = 0;
+    int id5x = 0;
+    int id6x = 0;
+    double xvalue = 0;
+    int numberDetections = 0;
+    double tickratio = 500 / 9.375;
+    boolean Position1 = false;
+    boolean Position2 = false;
+    boolean Position3 = false;
+    long Time = 0;
+
+    @Override
+
+    public void runOpMode() {
+
+        initTfod();
+        telemetry.addData("DS preview on/off", "3 dots, Camera Stream");
+        telemetry.addData(">", "Touch Play to start OpMode");
+        telemetry.update();
+
+        SampleMecanumDrive drive = new SampleMecanumDrive(this.hardwareMap,this.telemetry,direction);
+        sampleMecanumDrive.initialzeAttachments(hardwareMap);
+
+        Trajectory ZestyTrajectory = drive.trajectoryBuilder(new Pose2d())
+                .strafeLeft(10)
+                .build();
+        Trajectory ZestyTrajectory1 = drive.trajectoryBuilder(new Pose2d())
+                .forward(10)
+
+                .build();
+
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+        telemetryTfod();
+
+        telemetry.update();
+
+        tfodVisionPortal.close();
+
+        telemetry.update();
+        sleep(1000);
+
+        if (Position3 == true) {
+            telemetry.addData("pos 3", "");
+            //Position = 3
+        }
+        else if(Position2 == true){
+            telemetry.addData("pos 2","");
+            //Position = 2
+        }
+        else {
+            Position1 = true;
+            telemetry.addData("pos 1", "");
+            //Position = 1
+        }
+
+        initAprilTag();
+        aprilTag.getDetections();
+        List<AprilTagDetection> currentDetections = aprilTag.getDetections();
+        telemetry.addData("# AprilTags Detected", currentDetections.size());
+
+        numberDetections = currentDetections.size();
+        // Step through the list of detections and display info for each one.
+        for (AprilTagDetection detection : currentDetections) {
+            if (detection.metadata != null) {
+                telemetry.addLine(String.format("\n==== (ID %d) %s", detection.id, detection.metadata.name));
+                telemetry.addLine(String.format("XYZ %6.1f %6.1f %6.1f  (inch)", detection.ftcPose.x, detection.ftcPose.y, detection.ftcPose.z));
+                telemetry.addLine(String.format("PRY %6.1f %6.1f %6.1f  (deg)", detection.ftcPose.pitch, detection.ftcPose.roll, detection.ftcPose.yaw));
+                telemetry.addLine(String.format("RBE %6.1f %6.1f %6.1f  (inch, deg, deg)", detection.ftcPose.range, detection.ftcPose.bearing, detection.ftcPose.elevation));
+            } else {
+                telemetry.addLine(String.format("\n==== (ID %d) Unknown", detection.id));
+                telemetry.addLine(String.format("Center %6.0f %6.0f   (pixels)", detection.center.x, detection.center.y));
+            }
+
+
+
+            if (detection.id == 1){
+                id1x = (int) ((detection.ftcPose.x * tickratio) + (4.5 * tickratio));
+            }
+            if (detection.id == 2){
+                id2x = (int) ((detection.ftcPose.x * tickratio) + (4.5 * tickratio));
+            }
+            if (detection.id == 3){
+                id3x = (int) ((detection.ftcPose.x * tickratio) + (4.5 * tickratio));
+            }
+            if(detection.id == 4){
+                id4x = (int) ((detection.ftcPose.x * tickratio) + (4.6 * tickratio));
+            }
+            if (detection.id == 5){
+                id5x = (int) ((detection.ftcPose.x * tickratio) + (4.5 * tickratio));
+            }
+            if (detection.id == 6){
+                id6x = (int) ((detection.ftcPose.x * tickratio) + (4.4 * tickratio));
+            }
+
+
+            // end for() loop
+
+            // Add "key" information to telemetry
+            telemetry.addLine("\nkey:\nXYZ = X (Right), Y (Forward), Z (Up) dist.");
+            telemetry.addLine("PRY = Pitch, Roll & Yaw (XYZ Rotation)");
+            telemetry.addLine("RBE = Range, Bearing & Elevation");
+
+        }
+        while (numberDetections == 0) {
+
+            aprilTag.getDetections();
+
+
+            aprilTagDetections();
+
+            Time = Time + 1;
+            telemetry.addData("Times repeated:", Time);
+            telemetry.update();
+            if(Time == 600) {
+
+                break;
+            }
+        }
+
+        visionPortal.close();
+
+        if(Position1 == true){
+            telemetry.addData("move side", id4x);
+            telemetry.update();
+        }
+        else if (Position3 == true) {
+
+            telemetry.addData("move side", id6x);
+            telemetry.update();
+        }
+        else {
+
+        }
+
+
+
+        drive.followTrajectory(ZestyTrajectory);
+        drive.followTrajectory(ZestyTrajectory1);
+        drive.turn(90);
+    }
+
+    private void initAprilTag() {
+
+        // Create the AprilTag processor the easy way.
+        aprilTag = AprilTagProcessor.easyCreateWithDefaults();
+
+        // Create the vision portal the easy way.
+        if (USE_WEBCAM) {
+            visionPortal = VisionPortal.easyCreateWithDefaults(
+                    hardwareMap.get(WebcamName.class, "Webcam 1"), aprilTag);
+        } else {
+            visionPortal = VisionPortal.easyCreateWithDefaults(
+                    BuiltinCameraDirection.BACK, aprilTag);
+        }
+
+    }
+    private void initTfod() {
+
+        // Create the TensorFlow processor by using a builder.
+        tfod = new TfodProcessor.Builder()
+
+
+                .setModelAssetName(TFOD_MODEL_ASSET)
+                .setModelLabels(LABELS)
+                .setIsModelTensorFlow2(true)
+                .setModelInputSize(300)
+                .setModelAspectRatio(16.0 / 9.0)
+
+                .build();
+
+        // Create the vision portal by using a builder.
+        VisionPortal.Builder tfodBuilder = new VisionPortal.Builder();
+
+        // Set the camera (webcam vs. built-in RC phone camera).
+        if (USE_WEBCAM) {
+            tfodBuilder.setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"));
+        } else {
+            tfodBuilder.setCamera(BuiltinCameraDirection.BACK);
+        }
+        tfodBuilder.enableLiveView(true);
+        tfodBuilder.addProcessor(tfod);
+
+        tfodVisionPortal = tfodBuilder.build();
+        tfod.setMinResultConfidence(0.8f);
+        // Disable or re-enable the TFOD processor at any time.
+        tfodVisionPortal.setProcessorEnabled(tfod, true);
+
+
+    }
+    private void aprilTagDetections() {
+        List<AprilTagDetection> currentDetections = aprilTag.getDetections();
+        telemetry.addData("# AprilTags Detected", currentDetections.size());
+        numberDetections = currentDetections.size();
+
+        // Step through the list of detections and display info for each one.
+        for (AprilTagDetection detection : currentDetections) {
+            if (detection.metadata != null) {
+                telemetry.addLine(String.format("\n==== (ID %d) %s", detection.id, detection.metadata.name));
+                telemetry.addLine(String.format("XYZ %6.1f %6.1f %6.1f  (inch)", detection.ftcPose.x, detection.ftcPose.y, detection.ftcPose.z));
+                telemetry.addLine(String.format("PRY %6.1f %6.1f %6.1f  (deg)", detection.ftcPose.pitch, detection.ftcPose.roll, detection.ftcPose.yaw));
+                telemetry.addLine(String.format("RBE %6.1f %6.1f %6.1f  (inch, deg, deg)", detection.ftcPose.range, detection.ftcPose.bearing, detection.ftcPose.elevation));
+            } else {
+                telemetry.addLine(String.format("\n==== (ID %d) Unknown", detection.id));
+                telemetry.addLine(String.format("Center %6.0f %6.0f   (pixels)", detection.center.x, detection.center.y));
+            }
+
+
+            if (detection.id == 1){
+                id1x = (int) ((detection.ftcPose.x * tickratio) + (4.5 * tickratio));
+            }
+            if (detection.id == 2){
+                id2x = (int) ((detection.ftcPose.x * tickratio) + (4.5 * tickratio));
+            }
+            if (detection.id == 3){
+                id3x = (int) ((detection.ftcPose.x * tickratio) + (4.5 * tickratio));
+            }
+            if(detection.id == 4){
+                id4x = (int) ((detection.ftcPose.x * tickratio) + (4.7 * tickratio));
+            }
+            if (detection.id == 5){
+                id5x = (int) ((detection.ftcPose.x * tickratio) + (4.5 * tickratio));
+            }
+            if (detection.id == 6){
+                id6x = (int) ((detection.ftcPose.x * tickratio) + (4.3 * tickratio));
+            }
+
+        }
+        telemetry.addLine("\nkey:\nXYZ = X (Right), Y (Forward), Z (Up) dist.");
+        telemetry.addLine("PRY = Pitch, Roll & Yaw (XYZ Rotation)");
+        telemetry.addLine("RBE = Range, Bearing & Elevation");
+
+    }
+    private void telemetryTfod() {
+
+        List<Recognition> currentRecognitions = tfod.getRecognitions();
+        telemetry.addData("# Objects Detected", currentRecognitions.size());
+
+        // Step through the list of recognitions and display info for each one.
+        for (Recognition recognition : currentRecognitions) {
+            double x = (recognition.getLeft() + recognition.getRight()) / 2;
+            double y = (recognition.getTop() + recognition.getBottom()) / 2;
+
+            telemetry.addData("", " ");
+            telemetry.addData("Image", "%s (%.0f %% Conf.)", recognition.getLabel(), recognition.getConfidence() * 100);
+            telemetry.addData("- Position", "%.0f / %.0f", x, y);
+            telemetry.addData("- Size", "%.0f x %.0f", recognition.getWidth(), recognition.getHeight());
+            xvalue = recognition.getLeft();
+
+            if (xvalue <= 300) {
+                Position1 = false;
+                Position2 = true;
+                Position3 = false;
+                telemetry.addData("Position1 =", Position1);
+
+            } else if (xvalue > 300) {
+                Position3 = true;
+                Position2 = false;
+                Position1 = false;
+                telemetry.addData("Position3 =", Position3);
+            } else {
+                Position2 = false;
+                Position1 = true;
+                Position3 = false;
+                telemetry.addData("Position2 =", Position2);
+            }
+
+        }// end for() loop
+
+
+    }
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/SampleMecanumDrive.java
@@ -0,0 +1,370 @@
+package org.firstinspires.ftc.teamcode.drive;
+
+import androidx.annotation.NonNull;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.roadrunner.control.PIDCoefficients;
+import com.acmerobotics.roadrunner.drive.DriveSignal;
+import com.acmerobotics.roadrunner.drive.MecanumDrive;
+import com.acmerobotics.roadrunner.followers.HolonomicPIDVAFollower;
+import com.acmerobotics.roadrunner.followers.TrajectoryFollower;
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.acmerobotics.roadrunner.trajectory.TrajectoryBuilder;
+import com.acmerobotics.roadrunner.trajectory.constraints.AngularVelocityConstraint;
+import com.acmerobotics.roadrunner.trajectory.constraints.MecanumVelocityConstraint;
+import com.acmerobotics.roadrunner.trajectory.constraints.MinVelocityConstraint;
+import com.acmerobotics.roadrunner.trajectory.constraints.ProfileAccelerationConstraint;
+import com.acmerobotics.roadrunner.trajectory.constraints.TrajectoryAccelerationConstraint;
+import com.acmerobotics.roadrunner.trajectory.constraints.TrajectoryVelocityConstraint;
+import com.qualcomm.hardware.lynx.LynxModule;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.robotcore.hardware.PIDFCoefficients;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.TouchSensor;
+import com.qualcomm.robotcore.hardware.VoltageSensor;
+import com.qualcomm.robotcore.hardware.configuration.typecontainers.MotorConfigurationType;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
+import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequenceBuilder;
+import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequenceRunner;
+import org.firstinspires.ftc.teamcode.util.LynxModuleUtil;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.firstinspires.ftc.teamcode.drive.DriveConstants.MAX_ACCEL;
+import static org.firstinspires.ftc.teamcode.drive.DriveConstants.MAX_ANG_ACCEL;
+import static org.firstinspires.ftc.teamcode.drive.DriveConstants.MAX_ANG_VEL;
+import static org.firstinspires.ftc.teamcode.drive.DriveConstants.MAX_VEL;
+import static org.firstinspires.ftc.teamcode.drive.DriveConstants.MOTOR_VELO_PID;
+import static org.firstinspires.ftc.teamcode.drive.DriveConstants.RUN_USING_ENCODER;
+import static org.firstinspires.ftc.teamcode.drive.DriveConstants.TRACK_WIDTH;
+import static org.firstinspires.ftc.teamcode.drive.DriveConstants.encoderTicksToInches;
+import static org.firstinspires.ftc.teamcode.drive.DriveConstants.kA;
+import static org.firstinspires.ftc.teamcode.drive.DriveConstants.kStatic;
+import static org.firstinspires.ftc.teamcode.drive.DriveConstants.kV;
+
+/*
+ * Simple mecanum drive hardware implementation for REV hardware.
+ */
+@Config
+public class SampleMecanumDrive extends MecanumDrive {
+    public static PIDCoefficients TRANSLATIONAL_PID = new PIDCoefficients(0, 0, 0);
+    public static PIDCoefficients HEADING_PID = new PIDCoefficients(0, 0, 0);
+
+    public static double LATERAL_MULTIPLIER = 60/43.6;
+
+    public static double VX_WEIGHT = 1;
+    public static double VY_WEIGHT = 1;
+    public static double OMEGA_WEIGHT = 1;
+    double reset_angle = 0;
+
+    private TrajectorySequenceRunner trajectorySequenceRunner;
+
+    private static final TrajectoryVelocityConstraint VEL_CONSTRAINT = getVelocityConstraint(MAX_VEL, MAX_ANG_VEL, TRACK_WIDTH);
+    private static final TrajectoryAccelerationConstraint ACCEL_CONSTRAINT = getAccelerationConstraint(MAX_ACCEL);
+
+    private TrajectoryFollower follower;
+
+    protected TouchSensor touchSensor = null;
+    //    protected ColorSensor colorSensor1 = null;
+    protected DcMotor front_left_wheel = null;
+    protected DcMotor front_right_wheel = null;
+    protected DcMotor back_left_wheel = null;
+    protected DcMotor back_right_wheel = null;
+    protected DcMotor left_slide_motor = null;
+    protected DcMotor right_slide_motor = null;
+    protected Servo droneLauncher = null;
+    protected Servo armServo = null;
+    protected Servo claw = null;
+    protected Servo armServo2 = null;
+    private DcMotorEx leftFront, leftRear, rightRear, rightFront;
+    private List<DcMotorEx> motors;
+
+
+    private VoltageSensor batteryVoltageSensor;
+
+        private SampleMecanumDrive driveTrain;
+        private final Telemetry telemetry;
+
+    private List<Integer> lastEncPositions = new ArrayList<>();
+    private List<Integer> lastEncVels = new ArrayList<>();
+
+    public SampleMecanumDrive(HardwareMap hardwareMap, Telemetry telemetry, DirectionEnum direction) {
+        super(kV, kA, kStatic, TRACK_WIDTH, TRACK_WIDTH, LATERAL_MULTIPLIER);
+        this.telemetry = telemetry;
+
+        follower = new HolonomicPIDVAFollower(HEADING_PID, TRANSLATIONAL_PID, TRANSLATIONAL_PID,
+                new Pose2d(Math.toRadians(5.0), 0.5, 0.5), 0.5);
+
+        LynxModuleUtil.ensureMinimumFirmwareVersion(hardwareMap);
+
+        batteryVoltageSensor = hardwareMap.voltageSensor.iterator().next();
+
+        for (LynxModule module : hardwareMap.getAll(LynxModule.class)) {
+            module.setBulkCachingMode(LynxModule.BulkCachingMode.AUTO);
+        }
+
+        // TODO: adjust the names of the following hardware devices to match your configuration
+
+
+
+        leftFront = hardwareMap.get(DcMotorEx.class, "back_left_wheel");
+            leftRear = hardwareMap.get(DcMotorEx.class, "front_left_wheel");
+            rightRear = hardwareMap.get(DcMotorEx.class, "back_right_wheel");
+            rightFront = hardwareMap.get(DcMotorEx.class, "front_right_wheel");
+
+        motors = Arrays.asList(leftFront, leftRear, rightRear, rightFront);
+
+        for (DcMotorEx motor : motors) {
+            MotorConfigurationType motorConfigurationType = motor.getMotorType().clone();
+            motorConfigurationType.setAchieveableMaxRPMFraction(1.0);
+            motor.setMotorType(motorConfigurationType);
+        }
+
+
+        setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftFront.setDirection(DcMotorSimple.Direction.REVERSE);
+        leftRear.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        List<Integer> lastTrackingEncPositions = new ArrayList<>();
+        List<Integer> lastTrackingEncVels = new ArrayList<>();
+
+        // TODO: if desired, use setLocalizer() to change the localization method
+        setLocalizer(new StandardTrackingWheelLocalizer(hardwareMap, lastTrackingEncPositions, lastTrackingEncVels));
+
+
+        trajectorySequenceRunner = new TrajectorySequenceRunner(
+                follower, HEADING_PID, batteryVoltageSensor,
+                lastEncPositions, lastEncVels, lastTrackingEncPositions, lastTrackingEncVels
+        );
+    }
+    public void initialzeAttachments(HardwareMap hardwareMap) {
+        left_slide_motor = hardwareMap.dcMotor.get("leftSlideMotor");
+        right_slide_motor = hardwareMap.dcMotor.get("rightSlideMotor");
+        armServo = hardwareMap.servo.get("armServo");
+        claw = hardwareMap.servo.get("finger");
+        droneLauncher = hardwareMap.servo.get("launcher");
+//        colorSensor1 = hardwareMap.colorSensor.get("colorSensor1");
+//        colorSensor2 = hardwareMap.colorSensor.get("colorSensor2");
+        touchSensor = hardwareMap.touchSensor.get("touchSensor");
+        armServo2 = hardwareMap.servo.get("armServo2");
+    }
+    public enum DirectionEnum {
+        NORTH(90), SOUTH(-90), EAST(180), WEST(0);
+        private double correction;
+        DirectionEnum(double correction) {this.correction = correction;}
+        public double getCorrection() {return correction;}
+    }
+    public void reset_angle() {this.reset_angle = driveTrain.getExternalHeading() + this.reset_angle;}
+
+    public void linearSlideMove(int distance, double power, boolean rampDown) {
+
+        int leftSlidePos = left_slide_motor.getCurrentPosition();
+        int rightSlidePos = right_slide_motor.getCurrentPosition();
+
+        double rampDownPercent = 0.8;
+
+        left_slide_motor.setTargetPosition(leftSlidePos + distance);
+        right_slide_motor.setTargetPosition(rightSlidePos - distance);
+
+
+        left_slide_motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        right_slide_motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        left_slide_motor.setPower(power);
+        right_slide_motor.setPower(power);
+        left_slide_motor.setPower(0);
+        right_slide_motor.setPower(0);
+
+    }
+    public TrajectoryBuilder trajectoryBuilder(Pose2d startPose) {
+        return new TrajectoryBuilder(startPose, VEL_CONSTRAINT, ACCEL_CONSTRAINT);
+    }
+
+    public TrajectoryBuilder trajectoryBuilder(Pose2d startPose, boolean reversed) {
+        return new TrajectoryBuilder(startPose, reversed, VEL_CONSTRAINT, ACCEL_CONSTRAINT);
+    }
+
+    private void sleep(long ms) {
+        try {
+            Thread.sleep(ms);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+    public TrajectoryBuilder trajectoryBuilder(Pose2d startPose, double startHeading) {
+        return new TrajectoryBuilder(startPose, startHeading, VEL_CONSTRAINT, ACCEL_CONSTRAINT);
+    }
+
+    public TrajectorySequenceBuilder trajectorySequenceBuilder(Pose2d startPose) {
+        return new TrajectorySequenceBuilder(
+                startPose,
+                VEL_CONSTRAINT, ACCEL_CONSTRAINT,
+                MAX_ANG_VEL, MAX_ANG_ACCEL
+        );
+    }
+
+    public void turnAsync(double angle) {
+        trajectorySequenceRunner.followTrajectorySequenceAsync(
+                trajectorySequenceBuilder(getPoseEstimate())
+                        .turn(angle)
+                        .build()
+        );
+    }
+
+    public void turn(double angle) {
+        turnAsync(angle);
+        waitForIdle();
+    }
+
+    public void followTrajectoryAsync(Trajectory trajectory) {
+        trajectorySequenceRunner.followTrajectorySequenceAsync(
+                trajectorySequenceBuilder(trajectory.start())
+                        .addTrajectory(trajectory)
+                        .build()
+        );
+    }
+
+    public void followTrajectory(Trajectory trajectory) {
+        followTrajectoryAsync(trajectory);
+        waitForIdle();
+    }
+
+    public void followTrajectorySequenceAsync(TrajectorySequence trajectorySequence) {
+        trajectorySequenceRunner.followTrajectorySequenceAsync(trajectorySequence);
+    }
+
+    public void followTrajectorySequence(TrajectorySequence trajectorySequence) {
+        followTrajectorySequenceAsync(trajectorySequence);
+        waitForIdle();
+    }
+
+    public Pose2d getLastError() {
+        return trajectorySequenceRunner.getLastPoseError();
+    }
+
+    public void update() {
+        updatePoseEstimate();
+        DriveSignal signal = trajectorySequenceRunner.update(getPoseEstimate(), getPoseVelocity());
+        if (signal != null) setDriveSignal(signal);
+    }
+
+    public void waitForIdle() {
+        while (!Thread.currentThread().isInterrupted() && isBusy())
+            update();
+    }
+
+    public boolean isBusy() {
+        return trajectorySequenceRunner.isBusy();
+    }
+
+    public void setMode(DcMotor.RunMode runMode) {
+        for (DcMotorEx motor : motors) {
+            motor.setMode(runMode);
+        }
+    }
+
+    public void setZeroPowerBehavior(DcMotor.ZeroPowerBehavior zeroPowerBehavior) {
+        for (DcMotorEx motor : motors) {
+            motor.setZeroPowerBehavior(zeroPowerBehavior);
+        }
+    }
+
+    public void setPIDFCoefficients(DcMotor.RunMode runMode, PIDFCoefficients coefficients) {
+        PIDFCoefficients compensatedCoefficients = new PIDFCoefficients(
+                coefficients.p, coefficients.i, coefficients.d,
+                coefficients.f * 12 / batteryVoltageSensor.getVoltage()
+        );
+
+        for (DcMotorEx motor : motors) {
+            motor.setPIDFCoefficients(runMode, compensatedCoefficients);
+        }
+    }
+
+    public void setWeightedDrivePower(Pose2d drivePower) {
+        Pose2d vel = drivePower;
+
+        if (Math.abs(drivePower.getX()) + Math.abs(drivePower.getY())
+                + Math.abs(drivePower.getHeading()) > 1) {
+            // re-normalize the powers according to the weights
+            double denom = VX_WEIGHT * Math.abs(drivePower.getX())
+                    + VY_WEIGHT * Math.abs(drivePower.getY())
+                    + OMEGA_WEIGHT * Math.abs(drivePower.getHeading());
+
+            vel = new Pose2d(
+                    VX_WEIGHT * drivePower.getX(),
+                    VY_WEIGHT * drivePower.getY(),
+                    OMEGA_WEIGHT * drivePower.getHeading()
+            ).div(denom);
+        }
+
+        setDrivePower(vel);
+    }
+
+    @NonNull
+    @Override
+    public List<Double> getWheelPositions() {
+        lastEncPositions.clear();
+
+        List<Double> wheelPositions = new ArrayList<>();
+        for (DcMotorEx motor : motors) {
+            int position = motor.getCurrentPosition();
+            lastEncPositions.add(position);
+            wheelPositions.add(encoderTicksToInches(position));
+        }
+        return wheelPositions;
+    }
+
+    @Override
+    public List<Double> getWheelVelocities() {
+        lastEncVels.clear();
+
+        List<Double> wheelVelocities = new ArrayList<>();
+        for (DcMotorEx motor : motors) {
+            int vel = (int) motor.getVelocity();
+            lastEncVels.add(vel);
+            wheelVelocities.add(encoderTicksToInches(vel));
+        }
+        return wheelVelocities;
+    }
+
+    @Override
+    public void setMotorPowers(double v, double v1, double v2, double v3) {
+        leftFront.setPower(v);
+        leftRear.setPower(v1);
+        rightRear.setPower(v2);
+        rightFront.setPower(v3);
+    }
+
+    @Override
+    public double getRawExternalHeading() {
+        return 0;
+    }
+
+    @Override
+    public Double getExternalHeadingVelocity() {
+        return (double) 0.0;
+    }
+
+    public static TrajectoryVelocityConstraint getVelocityConstraint(double maxVel, double maxAngularVel, double trackWidth) {
+        return new MinVelocityConstraint(Arrays.asList(
+                new AngularVelocityConstraint(maxAngularVel),
+                new MecanumVelocityConstraint(maxVel, trackWidth)
+        ));
+    }
+
+    public static TrajectoryAccelerationConstraint getAccelerationConstraint(double maxAccel) {
+        return new ProfileAccelerationConstraint(maxAccel);
+    }
+}
